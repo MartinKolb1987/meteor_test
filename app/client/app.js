0775cf2481b564b9ec42e29658dbf28176b5d959
Martin Kolb <martin.kolb@mkolb.de>
2014-06-23 16:59:55 +0200

    added edit and delete function

--- app/client/app.js
+++ app/client/app.js
@@ -4,29 +4,52 @@ Movies = new Meteor.Collection("movies");
 // Bind moviesTemplate to Movies collection
 Template.moviesTemplate.movies = function () {
 	var myMovies = Movies.find().fetch();
-	// console.log(myMovies);
     return Movies.find().fetch();
 };
 
 Template.moviesTemplate.events({
-	'click .item': function () {
-	// console.log(this._id);
-	// Users.update(this._id, {$inc: {score: 2}});
-	},
 	'click .remove-item': function () {
 		Movies.remove(this._id);
+	},
+	'click .edit-item': function (e) {
+		helper.classToggler(e);
+	},
+	'click .submit-cancel': function (e) {
+		helper.classToggler(e);
+	},
+	'click .submit-edit-save': function (e) {
+		var clickedItem = $(e.currentTarget);
+		var title = clickedItem.siblings('input[name=title]').val();
+		var director = clickedItem.siblings('input[name=director]').val();
+
+		// update db entry
+		Movies.update(this._id, { title: title, director: director });
+
+		helper.classToggler(e);
 	}
 });
+
+var helper = {
+	classToggler: function(e){
+		var clickedItem = $(e.currentTarget);
+		clickedItem.parents('.item').find('.edit-fields').toggleClass('hide');
+		clickedItem.parents('.item').find('.show-fields').toggleClass('hide');
+	}
+};
 
 Template.addForm.events({
 	'submit form': function (e, template) {
 		e.preventDefault();
-		var title = template.find("input[name=title]").value;
-		var director = template.find("input[name=director]").value;
-		// console.log();
-		// console.log($(this).serializeArray());
-		// console.log('bingo');
-		// Movies.remove(this._id);
+
+		// get values
+		var title = template.find('input[name=title]').value;
+		var director = template.find('input[name=director]').value;
+		
+		// clear input fields
+		template.find('input[name=title]').value = '';
+		template.find('input[name=director]').value = '';
+
+		// inser movies
 		Movies.insert({ title: title, director: director });
 	}
 });
