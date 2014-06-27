38adb83a20fb66649d437beb596912a8d4127d09
Martin Kolb <martin.kolb@mkolb.de>
2014-06-27 13:29:42 +0200

    .

--- boilerplate/client/routes/home_route.js
+++ boilerplate/client/routes/home_route.js
@@ -1,5 +1,5 @@
-Router.map(function() {
-  this.route('home', {path: '/'});
-  // this.route('home', {path: '/about'});
-});
+// Router.map(function() {
+//   this.route('home', {path: '/'});
+//   // this.route('home', {path: '/about'});
+// });
 
